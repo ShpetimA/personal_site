fdb8027798f5d8646fe1b173ea330604646a9b51
Shpetim <shpetim.alimi@ndbit.net>
2025-04-10 16:18:16 +0200

    fix urls for blogs and fixed initial page styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://shpetimA.github.io',
+    base: 'personal_site',
     vite: {
         plugins: [tailwindcss()]
     }
