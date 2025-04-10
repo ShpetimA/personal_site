5dffea25c773f46cff568abbeb5ffa3ab6983010
Shpetim <shpetim.alimi@ndbit.net>
2025-04-10 15:57:09 +0200

    github actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://shpetimA.github.io',
+    base: 'personal_site',
     vite: {
         plugins: [tailwindcss()]
     }
